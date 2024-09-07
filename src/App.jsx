f2defbe6aded3385808b6639e1a025373ef05814
lgramajo99 <lucianongramajo@gmail.com>
2024-09-07 19:23:17 -0300

    configuracion base lista.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello World!</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident necessitatibus reprehenderit laboriosam nemo aspernatur, maxime fuga beatae aut, similique nam quas? Qui fuga in molestiae omnis molestias exercitationem doloribus tempora blanditiis itaque debitis. Nostrum ipsum sed labore corrupti nam maxime, facilis veritatis ex eos laborum corporis quia aliquam blanditiis perferendis?</p>
+
     </>
   )
 }
