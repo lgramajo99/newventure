9d3441317fb5b3c56e783dcd575ae82588c43338
lgramajo99 <lucianongramajo@gmail.com>
2024-09-08 03:11:30 -0300

    tailwind configurado exitosamente.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
   return (
     <>
       <div>
         <img src={viteLogo} className="logo" alt="Vite logo" />
         <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Hello World!</h1>
+      <h1 className='text-red-600'>Hello, World!</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident necessitatibus reprehenderit laboriosam nemo aspernatur, maxime fuga beatae aut, similique nam quas? Qui fuga in molestiae omnis molestias exercitationem doloribus tempora blanditiis itaque debitis. Nostrum ipsum sed labore corrupti nam maxime, facilis veritatis ex eos laborum corporis quia aliquam blanditiis perferendis?</p>
 
     </>
