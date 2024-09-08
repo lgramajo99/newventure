09f4e4d2ac47d2865be95c1da3e664f54042eb18
lgramajo99 <lucianongramajo@gmail.com>
2024-09-08 19:03:19 -0300

    React redux toolkit instalado y listo para usarse

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/layouts/Header'
 import Footer from './components/layouts/Footer'
+import Header from './components/layouts/Header'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div>
       <Header />
-      <div>
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </div>
-      <h1 className='text-red-600'>Hello, World!</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident necessitatibus reprehenderit laboriosam nemo aspernatur, maxime fuga beatae aut, similique nam quas? Qui fuga in molestiae omnis molestias exercitationem doloribus tempora blanditiis itaque debitis. Nostrum ipsum sed labore corrupti nam maxime, facilis veritatis ex eos laborum corporis quia aliquam blanditiis perferendis?</p>
+      <Home />
       <Footer />
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,20 @@
+import React from 'react'
+import reactLogo from '../assets/react.svg'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../store/features/counter/counterSlice';
+
+
 function Home() {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+
+
     return (<div>
         <h1>Pagina de inicio</h1>
+
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>Incrementar</button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>Decrementar</button>
     </div>)
 }
 
