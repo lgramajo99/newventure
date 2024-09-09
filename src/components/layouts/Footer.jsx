c6c0656228e66c8cf7d79bf9839a64c57f51be11
lgramajo99 <lucianongramajo@gmail.com>
2024-09-09 00:36:46 -0300

    newsletter listo

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,5 +1,9 @@
+import FooterNewsletter from "./FooterNewsletter";
+
 function Footer() {
     return (<footer>
+        <FooterNewsletter />
+
         <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti placeat mollitia, provident unde non nemo ipsam totam porro odio assumenda?</p>
     </footer>)
 }
---
+++ src/components/layouts/FooterNewsletter.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+function FooterNewsletter() {
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Email enviado:", email);
+        alert(`Correo registrado con exito:  ${email}`)
+    };
+
+    return (
+        <form
+            className="text-xl bg-red-200 py-4 px-8 mx-auto shadow-lg text-center"
+            method="post"
+            onSubmit={handleSubmit}>
+
+            <p>
+                Suscríbete a nuestro boletín para inspirarte, obtener consejos y trucos y ser el primero en conocer nuevos productos y lanzamientos de colores.
+            </p>
+            <label
+                hidden
+                htmlFor="email">
+                Correo electrónico
+            </label>
+
+            <input
+                aria-label="Correo electrónico"
+                aria-required="true"
+                autoComplete="email"
+                autoFocus
+                className="px-6 py-3 m-1 rounded-full text-lg border-none focus:outline-none focus:ring-2 focus:ring-red-500"
+                id="email"
+                inputMode="email"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="email"
+                required
+                title="Correo electrónico"
+                type="email"
+                value={email}
+            />
+            <button
+                aria-label="Unirse al boletín"
+                className="p-3 m-1 bg-red-500 rounded-full text-lg font-semibold hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500  disabled:cursor-not-allowed"
+                disabled={email === ""}
+                title="Unirse al boletin"
+                type="submit">
+                Unirse
+            </button>
+        </form >
+    )
+}
+
+export default FooterNewsletter;
