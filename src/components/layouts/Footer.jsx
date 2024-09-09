4699590ec0181d87c784db4dcb800c23c01b3d3d
lgramajo99 <lucianongramajo@gmail.com>
2024-09-09 14:57:31 -0300

    seccion de social footer lista, falta agregar las url correspondientes

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,9 +1,10 @@
 import FooterNewsletter from "./FooterNewsletter";
+import FooterSocial from "./FooterSocial";
 
 function Footer() {
     return (<footer>
         <FooterNewsletter />
-
+        <FooterSocial />
         <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti placeat mollitia, provident unde non nemo ipsam totam porro odio assumenda?</p>
     </footer>)
 }
