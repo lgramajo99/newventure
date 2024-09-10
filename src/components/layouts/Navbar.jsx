d8661456a88f587ab5c5385c592f7baf051af0df
lgramajo99 <lucianongramajo@gmail.com>
2024-09-10 16:36:38 -0300

    dando retoques visuales.

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -8,22 +10,40 @@ export default function Navbar() {
     }
 
     return (
-        <nav className="bg-red-300 shadow-md">
+        <nav className="bg-red-300 shadow-lg border-red-400">
             <div className="mx-auto">
                 <div className="flex justify-between items-center py-2 px-4">
-                    <h3>Formulario: Search🔎</h3>
+                    <span className='hidden md:block'>Formulario: Search🔎</span>
+                    <span className='md:hidden'></span>
+
                     <h1 className="text-lg font-bold">LOL</h1>
-                    <div className='hidden md:block'>
-                        <span>🛒</span>
-                        <span>❤</span>
-                        <span>👦</span>
+
+                    <div className='hidden md:block space-x-4'>
+                        <Link to={'#'} className='no-underline'>🛒</Link >
+                        <Link to={'#'} className='no-underline'>❤</Link >
+                        <Link to={'#'} className='no-underline'>👦</Link >
                     </div>
 
-                    <button className="md:hidden focus:outline-none" onClick={toggleMenu} aria-label="Toggle menu">
-                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                    <button
+                        className="md:hidden flex items-center justify-center p-1 rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition duration-150 ease-in-out cursor-pointer"
+                        onClick={toggleMenu}
+                        aria-label="Toggle menu"
+                    >
+                        <svg
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M4 6h16M4 12h16M4 18h16"
+                            />
                         </svg>
                     </button>
+
 
                 </div>
                 <hr className='w-11/12 mx-auto border-red-300' />
@@ -31,13 +51,15 @@ export default function Navbar() {
                     <ul className="md:flex md:flex-wrap md:justify-around list-none w-full">
                         {['Home', 'About', 'Services', 'Contact', 'Blog', 'Portfolio', 'FAQ', 'Support', 'Careers', 'Terms', 'Privacy'].map((item) => (
                             <li key={item} className=" md:w-1/6 text-end md:text-center">
-                                <a href="#" className="text-customGray hover:text-black">{item}</a>
+                                <Link to="#" className="no-underline text-customGray hover:text-black">{item}</Link>
                             </li>
                         ))}
+                        <span className='md:hidden text-end'>Formulario: Search🔎</span>
+
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav >
     );
 
 }
