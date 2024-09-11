12c944108d267e4a30fb9bc877f1704bb844578a
lgramajo99 <lucianongramajo@gmail.com>
2024-09-11 15:41:49 -0300

    mejorando el carousel

--- src/components/common/CarouselCards.jsx
+++ src/components/common/CarouselCards.jsx
@@ -6,12 +6,10 @@ function CarouselCards() {
     const nextSlides = () => { setCountIndex(countIndex + 1) };
     const prevSlides = () => { setCountIndex(countIndex - 1) };
 
-    return (<div>
-        <h1>Mas vendidos</h1>
-
-        <h1>cantidad de card: {countIndex}</h1>
-        <button type="button" onClick={prevSlides}>anterior</button>
-        <button type="button" onClick={nextSlides}>siguiente</button>
+    return (<div className="bg-slate-300 w-full  relative overflow-hidden">
+        <button type="button" className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-lg" onClick={prevSlides}>&lt;</button>
+        <h1 className="text-center">cantidad de card: {countIndex}</h1>
+        <button type="button" className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-lg" onClick={nextSlides}> &gt;</button>
 
     </div>)
 }
